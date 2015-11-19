257dee3ddc168dcfce509ab812e4b4d4b6001ba3
Isaac <yzaak.silva@gmail.com>
2015-11-19 13:16:36 -0300

    Main
    Código do main. Responsável por iniciar a leitura do AsinExec numa
    Thread e de determinar o tempo de leitura dos dados vindos da porta
    serial/Arduino

---
+++ javaApp/src/br/edu/ifba/embarcados/javaApp/Executor.java
@@ -0,0 +1,22 @@
+package br.edu.ifba.embarcados.javaApp;
+
+import br.edu.ifba.embarcados.javaApp.asincexec.AsincExec;
+
+public class Executor{
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		ListenerMagnetometro listener = new ListenerMagnetometro();
+		AsincExec asinc = new AsincExec("COM3");
+		asinc.addListener(listener); //notificacao do listener
+				
+		Thread t1 = new Thread(asinc);
+		
+		t1.start();
+		
+		for(int i=0;i<5000;i++)
+			Thread.sleep(500);
+		
+		t1.join();
+	}
+}
